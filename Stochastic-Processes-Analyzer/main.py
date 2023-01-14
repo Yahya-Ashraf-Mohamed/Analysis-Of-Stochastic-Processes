b52cf9b0b964d2d45ca02f0c49827aabf51ece54
Yahya-Ashraf-Mohamed <s-yahya.mohamed@zewailcity.edu.eg>
2023-01-14 10:11:41 +0200

    "Add Final GUI"

--- Stochastic-Processes-Analyzer/main.py
+++ Stochastic-Processes-Analyzer/main.py
@@ -7,6 +7,8 @@ from PIL import ImageTk, Image
 def Generate_Process():
     global FileName
     FileName = "D:\Self Development\Zewail collage material\Academic years\Year 3\Probability\Stochastic-Processes-Analyzer\Processes\Process_X.csv"
+
+    global Process_X
     Process_X = Functions.OpenFile(FileName)
     #Functions.Plot_Process(Process_X, "X[t] Graph", "Time [t]", "X[t]")
 
@@ -51,14 +53,19 @@ def Generate_Process():
 # Browse for input file then print its name in the text box
 def GetInputFile():
     global FileName
-    FileName = filedialog.askopenfilename(initialdir="Home", title="Open CSV File",
-                                          filetypes=(("csv Files", "*.csv"),))
-    Process_X = Functions.OpenFile(FileName)
-    if Process_X.any():
-        FileName_Label.configure(text=FileName)
-    else:
-        messagebox.showerror("ERROR", "Can't open file!\n")
+    try:
+        FileName = filedialog.askopenfilename(initialdir="Home", title="Open CSV File",
+                                              filetypes=(("csv Files", "*.csv"),))
 
+        Process_X = Functions.OpenFile(FileName)
+
+        if Process_X.any():
+            FileName_Label.delete(0, END)
+            FileName_Label.insert(0,FileName)
+        else:
+            messagebox.showerror("ERROR", "Can't open file!\n")
+    except:
+        messagebox.showerror("ERROR", "An Error Occurred!\n")
 
 def Click_Show_Plot_M_Ensemble_Button(key):
     # Open button
@@ -94,30 +101,52 @@ if __name__ == '__main__':
 
     # Main App
     root = Tk()
-    root.geometry("735x550")
+    root.geometry("420x380")
     root.title("Statistical Analysis Of Text Files")
 
-
+# ==================================================================================================================== #
 # Frame 1
-    Frame_1 = LabelFrame(root, padx=10, pady=22)
+    Frame_1 = LabelFrame(root, padx=0, pady=0)
 
     # Create a labelbox widget
-    FileName_Label = Label(Frame_1, text='', width=88, height=1, borderwidth=3, bg="White", fg="Black",
+    FileName_Label = Entry(Frame_1, width=55, borderwidth=5, bg="White", fg="Black",
                            font=('Helvetica', 10), justify="left")
-
+    FileName_Label.insert(0, "Default Process X generated.")
     # Create Buttons
     button_Choose_File = Button(Frame_1, text="Browse File", state=ACTIVE, padx=22, pady=2, fg="blue",
                                 command=lambda: GetInputFile())
 
 
 # Shoving Frame 1 into the screen
-    Frame_1.grid(row=0, column=0)
-    FileName_Label.grid(row=0, column=1)
-    button_Choose_File.grid(row=1, column=1)
+    Frame_1.place(x=10,y=10)
+    FileName_Label.pack()
+    button_Choose_File.pack()
 
+# ==================================================================================================================== #
+# Frame 6
+    Frame_6 = LabelFrame(root, padx=0, pady=0, text="Calculate")
 
+    # Output TextBox
+    Time_ACF_TextBox = Entry(Frame_6, width=65, borderwidth=5, bg="White", fg="Black", state=DISABLED)
+
+    # Create Buttons
+    Ensemble_Mean_Button = Button(Frame_6, text="Ensemble Mean", state=ACTIVE, padx=20, pady=2, fg="Black",
+                                  command=lambda: GetInputFile())
+    Time_ACF_Button = Button(Frame_6, text="Time_ACF", state=ACTIVE, padx=20, pady=2, fg="Black",
+                             command=lambda: GetInputFile())
+    Total_Average_Power_Button = Button(Frame_6, text="Total_Average_Power", state=ACTIVE, padx=20, pady=2, fg="Black",
+                                        command=lambda: GetInputFile())
+
+    # Shoving Frame 6 into the screen
+    Frame_6.place(x=10, y=75)
+    Time_ACF_TextBox.pack(side=TOP)
+    Ensemble_Mean_Button.pack(side=LEFT)
+    Time_ACF_Button.place(x=135, y=27)
+    Total_Average_Power_Button.pack(side=RIGHT)
+
+# ==================================================================================================================== #
 # Frame 2
-    Frame_2 = LabelFrame(root, padx=0, pady=0, text="Show M Ensembles of process")
+    Frame_2 = LabelFrame(root, padx=0, pady=0, text="Plot Show M Ensembles of process")
     # Ensemble Label
     Ensemble_Label = Label(Frame_2, text='Ensembles: ', width=10, height=1, borderwidth=3, bg="White", fg="Black",
                            font=('Helvetica', 10), justify="left")
@@ -130,17 +159,18 @@ if __name__ == '__main__':
                                 command=lambda: GetInputFile())
 
 # Shoving Frame 2 into the screen
-    Frame_2.grid(row=1, column=0)
-    Ensemble_Label.grid(row=0, column=0)
-    Ensemble_TextBox.grid(row=0, column=1)
-    Plot_M_Ensemble_Button.grid(row=0, column=2)
+    Frame_2.place(x=10, y=160)
+    Ensemble_Label.pack(side="left")
+    Ensemble_TextBox.pack(side="left")
+    Plot_M_Ensemble_Button.pack(side="right")
 
+# ==================================================================================================================== #
 # Frame 3
-    Frame_3 = LabelFrame(root, padx=0, pady=0, text="ACF")
+    Frame_3 = LabelFrame(root, padx=10, pady=10, text="Plot ACF")
     # ACF Label
-    ACF_i_Label = Label(Frame_3, text='i:', width=10, height=1, borderwidth=3, bg="White", fg="Black",
+    ACF_i_Label = Label(Frame_3, text='i:', width=5, height=1, borderwidth=3, bg="White", fg="Black",
                            font=('Helvetica', 10), justify="left")
-    ACF_j_Label = Label(Frame_3, text='j:', width=10, height=1, borderwidth=3, bg="White", fg="Black",
+    ACF_j_Label = Label(Frame_3, text='j:', width=5, height=1, borderwidth=3, bg="White", fg="Black",
                            font=('Helvetica', 10), justify="left")
 
     # Text box for ACF
@@ -154,12 +184,70 @@ if __name__ == '__main__':
                                     command=lambda: GetInputFile())
 
 # Shoving Frame 3 into the screen
-    Frame_3.grid(row=1, column=1)
+    Frame_3.place(x=260, y=160)
     ACF_i_Label.grid(row=0, column=0)
     ACF_i_TextBox.grid(row=0, column=1)
-    ACF_j_Label.grid(row=0, column=3)
-    ACF_j_TextBox.grid(row=0, column=4)
-    Plot_ACF_Button.grid(row=1, column=2)
 
-    # Generate main loop
+    ACF_j_Label.grid(row=1, column=0)
+    ACF_j_TextBox.grid(row=1, column=1)
+
+    Plot_ACF_Button.grid(row=2, column=1)
+
+# ==================================================================================================================== #
+# Frame 4
+    Frame_4 = LabelFrame(root, padx=0, pady=0, text="Plot Time Mean of N samples")
+    # Ensemble Label
+    Time_Mean_Label = Label(Frame_4, text='N Samples: ', width=10, height=1, borderwidth=3, bg="White", fg="Black",
+                           font=('Helvetica', 10), justify="left")
+    # Text box for M samples
+    Time_Mean_TextBox = Entry(Frame_4, width=10, borderwidth=5, bg="White", fg="Black")
+    Time_Mean_TextBox.bind("<Key>", Click_Show_Plot_M_Ensemble_Button)
+
+    # Create Buttons
+    Plot_N_Time_Mean_Button = Button(Frame_4, text="Plot", state=DISABLED, padx=22, pady=2, fg="Black",
+                                command=lambda: GetInputFile())
+
+# Shoving Frame 4 into the screen
+    Frame_4.place(x=10, y=238)
+    Time_Mean_Label.pack(side="left")
+    Time_Mean_TextBox.pack(side="left")
+    Plot_N_Time_Mean_Button.pack(side="right")
+
+# ==================================================================================================================== #
+# Frame 5
+    Frame_5 = LabelFrame(root, padx=0, pady=0, text="Plot Time ACF of N samples")
+    # Ensemble Label
+    Time_ACF_Label = Label(Frame_5, text='N Samples: ', width=10, height=1, borderwidth=3, bg="White", fg="Black",
+                           font=('Helvetica', 10), justify="left")
+    # Text box for M samples
+    Time_ACF_TextBox = Entry(Frame_5, width=10, borderwidth=5, bg="White", fg="Black")
+    Time_ACF_TextBox.bind("<Key>", Click_Show_Plot_M_Ensemble_Button)
+
+    # Create Buttons
+    Plot_N_Time_ACF_Button = Button(Frame_5, text="Plot", state=DISABLED, padx=22, pady=2, fg="Black",
+                                    command=lambda: GetInputFile())
+
+# Shoving Frame 5 into the screen
+    Frame_5.place(x=10, y=316)
+    Time_ACF_Label.pack(side="left")
+    Time_ACF_TextBox.pack(side="left")
+    Plot_N_Time_ACF_Button.pack(side="right")
+
+# ==================================================================================================================== #
+# Frame 6
+    Frame_7 = LabelFrame(root, padx=0, pady=0, text="Plot")
+
+    # Create Buttons
+    Ensemble_Mean_Button = Button(Frame_7, text="Ensemble Mean", state=ACTIVE, padx=20, pady=2, fg="Black",
+                                  command=lambda: GetInputFile())
+    Time_ACF_Button = Button(Frame_7, text="Time_ACF", state=ACTIVE, padx=35, pady=2, fg="Black",
+                             command=lambda: GetInputFile())
+
+    # Shoving Frame 6 into the screen
+    Frame_7.place(x=260, y=290)
+    Ensemble_Mean_Button.pack(side=TOP)
+    Time_ACF_Button.pack(side=BOTTOM)
+
+# ==================================================================================================================== #
+# Generate main loop
     root.mainloop()
